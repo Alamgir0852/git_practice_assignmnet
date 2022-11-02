da9266a22a567dc5a3228c772eeffcb98d34acb8
Amit Kumar Thakur <54702865+Amitaryabht@users.noreply.github.com>
2022-11-02 23:47:55 +0530

    Revert "prime and palindrome code fixed"

--- pallindrome.js
+++ pallindrome.js
@@ -1,7 +1,7 @@
 let str = "naman";
 let bag = "";
-for(i=str.length;i>=0;i--){
-   bag+=str[i];
+for(i=str.length;i--){
+   bag+=str[j];
 }if (str==bag){
 	console.log("Palindrome");
 else console.log("Not palindrome");
